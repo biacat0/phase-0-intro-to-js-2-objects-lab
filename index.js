b5f05b42a1f71e0fe4ae627d00ea8725f1896e49
June Biadog <jb.biadog@gmail.com>
2022-11-01 16:25:37 -0400

    commit tree bbeba7a3e0e25e94d1fee1c24d6471dd2173d63c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+// const employee = {
+//     employee 1: "John"
+//         line 1: "123 carol st"
+//         line 2: "apartment 2"
+//         city: "New York"
+//         state: "New York"
+//         zip: "11122"
+//     employee 2: "Jacob"
+//     employee 3: "Jasmine"
+// } 
